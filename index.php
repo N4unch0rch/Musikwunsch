4181ef67e00b8adbe38773f60fe02ea62bc12a34
N4unch0rch <jan@engel.saarland>
2016-09-02 15:39:09 +0200

    Code Beautify
    Design angepasst und Code Formatted

--- index.php
+++ index.php
@@ -21,70 +21,76 @@ if($_GET['like']!=''){
 ?>               
 <!DOCTYPE html>
 <html lang="de">
+
 <head>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="css/bootstrap.min.css">
-<link rel="stylesheet" href="css/custom.min.css">
-<script src="js/jquery.min.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/custom.min.css">
+    <script src="js/jquery.min.js"></script>
 </head>
+
 <body>
-			   <!-- Horizontal Form -->
-                <div class="panel panel-green margin-bottom-40">
-                    <div class="panel-heading">
-                        
+    <div class="col-md-2">&nbsp;</div>
+    <div class="col-md-8">
+        <!-- Horizontal Form -->
+        <div class="panel panel-green margin-bottom-40">
+            <div class="panel-heading">
+
+            </div>
+            <div class="panel-body">
+                <form class="form-horizontal" method="get" action="index.php">
+                    <input type="hidden" value="new" id="wunsch" name="wunsch">
+                    <div class="form-group">
+                        <label for="titel" class="col-lg-2 control-label">Titel</label>
+                        <div class="col-lg-10">
+                            <input type="text" class="form-control" name="titel" id="titel" placeholder="Titel">
+                        </div>
                     </div>
-                    <div class="panel-body">
-                        <form class="form-horizontal" method="get" action="index.php">
-							<input type="hidden" value="new" id="wunsch" name="wunsch">
-                            <div class="form-group">
-                                <label for="titel" class="col-lg-2 control-label">Titel</label>
-                                <div class="col-lg-10">
-                                    <input type="text" class="form-control" name="titel" id="titel" placeholder="Titel">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="interpret" class="col-lg-2 control-label">Interpret</label>
-                                <div class="col-lg-10">
-                                    <input type="text" class="form-control" name="interpret" id="interpret" placeholder="Interpret">
-                                </div>
-                            </div>
-                           <!-- <div class="form-group">
+                    <div class="form-group">
+                        <label for="interpret" class="col-lg-2 control-label">Interpret</label>
+                        <div class="col-lg-10">
+                            <input type="text" class="form-control" name="interpret" id="interpret" placeholder="Interpret">
+                        </div>
+                    </div>
+                    <!-- <div class="form-group">
                                 <label for="name" class="col-lg-2 control-label">Name</label>
                                 <div class="col-lg-10">
                                     <input type="text" class="form-control" name="name" id="name" placeholder="Name">
 									
                                 </div>
                             </div>-->
-                            <div class="form-group">
-                                <div class="col-lg-offset-2 col-lg-10">
-                                    <button type="submit" class="btn btn-success">Absenden</button>
-                                </div>
-                            </div>
-                        </form>
+                    <div class="form-group">
+                        <div class="col-lg-offset-2 col-lg-10">
+                            <button type="submit" class="btn btn-success">Absenden</button>
+                        </div>
                     </div>
-                </div>
-                <!-- End Horizontal Form -->
-				<?php if($_GET['list']=='likes') { echo '<a class="btn btn-info" style="color:white;" href="?">Nach Reihenfolge sortieren!</a>';}
-						else {echo '<a class="btn btn-info" style="color:white;" href="?list=likes">Nach Anzahl der Likes sortieren!</a>';} ?>
-<table class="table table-striped table-hover ">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>Titel</th>
-      <th>Interpret</th>
-	  <th>Voting</th>
-	  <th style="width:9%;">&nbsp;</th>
-    </tr>
-  </thead>
-  <tbody>
-  
-	<?php listwishes($_GET['list']); ?>
-      
-  </tbody>
-</table> 
-<!-- Latest compiled and minified JavaScript -->
-<script src="js/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+                </form>
+            </div>
+        </div>
+        <!-- End Horizontal Form -->
+        <?php if($_GET[ 'list']=='likes' ) { echo '<a class="btn btn-info" style="color:white;" href="?">Nach Reihenfolge sortieren!</a>';} else {echo '<a class="btn btn-info" style="color:white;" href="?list=likes">Nach Anzahl der Likes sortieren!</a>';} ?>
+        <table class="table table-striped table-hover ">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Titel</th>
+                    <th>Interpret</th>
+                    <th>Voting</th>
+                    <th style="width:9%;">&nbsp;</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php listwishes($_GET[ 'list']); ?>
+
+            </tbody>
+        </table>
+    </div>
+    <div class="col-md-2">&nbsp;</div>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 
 </body>
+
 </html>
